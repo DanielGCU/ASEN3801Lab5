97f8265a8d63c5586b4edebd33f7f855acb6d97d
DanielGCU <dagh5052@colorado.edu>
2025-11-11 08:56:31 -0700

    Create Lab5_main.m

---
+++ Lab5_main.m
@@ -0,0 +1,15 @@
+%{
+Author(s): Daniel Ghoreyshi, Ridley Avila, Matthew Buccio, Aleksei Suchkov
+
+Course: ASEN 3801 -- Aero Vehicle Dynamics and Controls Lab 5: Fixed-Wing Aircraft Simulation and Control
+
+Goal: Calculate aerodynamic forces and moments from the aircraft state, control surfaces, and
+aerodynamic coefficients. Determine modal characteristics from aircraft performance data
+
+%}
+
+%% Housekeeping 
+
+clc
+clear
+close all
